5c76f68302e3d77148b0f778611697a302755ef5
nicole <nicole.rogoff@gmail.com>
2018-02-11 11:01:30 -0600

    Add the changes of Nicole

--- js/source/comp/DashboardView.jsx
+++ js/source/comp/DashboardView.jsx
@@ -2,17 +2,18 @@ var DashboardView = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<div style={{textAlign: "center"}}>	
-					<h1 style={{marginLeft: 15}}>Cat Woof</h1>
-				</div>
-				<div style={{textAlign: "center"}}>  
+				
+				<div style={{textAlign: "center", border: "1px solid #ccc"}}>  
 					<img
-							src="http://moziru.com/images/drawn-narwhal-rainbow-12.jpg"
+							src="https://image.freepik.com/free-vector/unicorn-background-design_1324-79.jpg"
 							width={400} 
 							height={400} 
 					 />
 				</div>
-				<div style={{textAlign: "center", margin: "0 auto", width: 400}}> 
+				<div style={{textAlign: "center"}}>	
+					<h1 style={{marginLeft: 15}}>unicorn</h1>
+				</div>
+				<div style={{textAlign: "center", margin: "0 auto", width: 400, border: "1px solid #ccc"}}> 
 					<p>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 					</p>
